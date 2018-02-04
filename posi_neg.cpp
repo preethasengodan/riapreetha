b8468c590136e1f68e0dd1fe16743b002c8c045d
preethasengodan <35951727+preethasengodan@users.noreply.github.com>
2018-02-04 12:22:36 +0000

    Create posi_neg.cpp

---
+++ posi_neg.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int num;
+    cin>>num;
+    if(num>0)
+    {
+        cout<<"the number is positive";
+    }
+    else if(num<0)
+    {
+        cout<<"the numb is negative";
+    }
+    else 
+    {
+        cout<<"the number is zero";
+    }
+    
+}
